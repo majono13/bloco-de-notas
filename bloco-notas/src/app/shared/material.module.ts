fc7b1e450b3bc2a5254212030390b3a970f80748
Maria Joaquina <mariaklmn@outlook.com>
2022-11-23 20:13:26 -0300

    Inicialização do componente home

---
+++ bloco-notas/src/app/shared/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  imports: [],
+  exports: [MatButtonModule, MatToolbarModule],
+  declarations: [],
+  providers: [],
+})
+export class MaterialModule {}
